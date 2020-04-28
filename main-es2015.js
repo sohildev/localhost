28ad1f2e27f2c8585eb513706c2ba2240681d1d4
sohildev <58003771+sohildev@users.noreply.github.com>
2020-04-28 11:51:31 +0530

    'link-change'

--- main-es2015.js
+++ main-es2015.js
@@ -2210,7 +2210,7 @@ __webpack_require__.r(__webpack_exports__);
 const environment = {
     production: false,
     // APIEndpoint: 'http://192.168.1.138:9005/api',
-    APIEndpoint: 'https://1fcecd73.ngrok.io/api'
+    APIEndpoint: 'https://4d98cd38.ngrok.io/api'
 };
 /*
  * For easier debugging in development mode, you can import the following file
